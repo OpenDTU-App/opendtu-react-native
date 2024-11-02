c9a9502543844cf128103d8d7637c581cf15cb0e
CommanderRedYT <ccommanderred@gmail.com>
2024-11-02 16:46:08 +0100

    Make more updates for fixing compilation

--- src/github/index.tsx
+++ src/github/index.tsx
@@ -1,7 +1,7 @@
 import type { FC, PropsWithChildren } from 'react';
 import { createContext, useContext, useMemo } from 'react';
 
-import { Octokit } from 'octokit';
+import { Octokit } from '@octokit/core';
 
 export const GithubContext = createContext<Octokit | undefined>(undefined);
 
