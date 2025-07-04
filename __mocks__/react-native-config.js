0f4d8204b67f05f61a35e51648fd723a4a175412
CommanderRedYT <ccommanderred@gmail.com>
2025-07-04 21:10:06 +0200

    Remove unused testing framework

--- __mocks__/react-native-config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('react-native-config').Config} */
-const config = {
-  DISABLE_IN_APP_UPDATES: 'false',
-};
-
-export default config;
--- __mocks__/react-native-share.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-/* eslint-env jest */
-
-jest.mock('react-native-share', () => ({
-  default: jest.fn(),
-}));
